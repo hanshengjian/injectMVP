b7dbf513090f5d2a9f475f111fc37b05f5c18c6d
jose.han <jose.han@dingtone.me>
2019-07-03 16:59:39 +0800

    add 代理InjectManager

--- inject-api/src/main/java/com/ca/api/InjectManager.java
+++ inject-api/src/main/java/com/ca/api/InjectManager.java
@@ -20,55 +20,19 @@ import java.lang.reflect.Method;
  */
 public class InjectManager {
 
-    private final static String TAG = "InjectManager";
     private static volatile InjectManager mInstance;
-    private boolean initSucc = true;
     private InjectManager() {
+        mInjectManager_ = InjectManager_.getInstance();
     }
 
+    private InjectManager_ mInjectManager_;
+
     public void init(Application application) {
-        try {
-            Class<?> threadClazz = Class.forName(Const.PACKGE_NAME + "." + Const.CLASS_NAME);
-            Method method = threadClazz.getMethod("init");
-            method.invoke(null);
-        } catch (Exception e) {
-            initSucc = false;
-            Log.i(TAG, "init failed :" + e.getMessage());
-        }
+        mInjectManager_.init(application);
     }
 
     public void inject(Object obj) {
-        if (!initSucc) {
-            return;
-        }
-        Field[] fields = obj.getClass().getDeclaredFields();
-        int length = fields.length;
-        for (int i = 0; i < length; i++) {
-            Field field = fields[i];
-            if (field.isAnnotationPresent(AutoWired.class)) {
-                Object instance;
-                try {
-                    String key = field.getType().getName();
-                    Class<?> threadClazz = Class.forName(Const.PACKGE_NAME + "." + Const.CLASS_NAME);
-                    Method method = threadClazz.getMethod("getServiceImpl", String.class);
-                    Meta meta = (Meta) method.invoke(null,key);
-                    instance = Class.forName(meta.getPath()).newInstance();
-                    field.setAccessible(true);
-                    field.set(obj, instance);
-                } catch (InstantiationException e) {
-                    Log.i(TAG,"inject failed: " + e.getMessage());
-                } catch (IllegalAccessException e) {
-                    Log.i(TAG,"inject failed: " + e.getMessage());
-                } catch (ClassNotFoundException e) {
-                    Log.i(TAG,"inject failed: " + e.getMessage());
-                } catch (NoSuchMethodException e) {
-                    Log.i(TAG,"inject failed: " + e.getMessage());
-                } catch (InvocationTargetException e) {
-                    Log.i(TAG,"inject failed: " + e.getMessage());
-                }
-
-            }
-        }
+        mInjectManager_.inject(obj);
     }
 
     public static InjectManager getInstance() {
